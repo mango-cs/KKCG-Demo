2a4a7a65e4d5609a7674471a32e6908133dd4518
DarkAvenger420 <neerajsaranala312@gmail.com>
2025-06-15 16:15:53 +0100

    Fix import error: Update utils/__init__.py to import correct function names from heatmap_utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,16 +2,18 @@
 # This package contains utility modules for the KKCG multipage Streamlit app
 
 from .data_simulation import generate_demand_data, SOUTH_INDIAN_DISHES, OUTLETS
-from .heatmap_utils import generate_heatmap, generate_trend_chart, generate_comparison_bar_chart
+from .heatmap_utils import create_demand_heatmap, generate_trend_chart, generate_comparison_bar_chart, generate_ai_insights, get_performance_metrics
 from .insights import compute_business_insights, generate_insight_texts, generate_recommendations
 
 __all__ = [
     'generate_demand_data',
     'SOUTH_INDIAN_DISHES', 
     'OUTLETS',
-    'generate_heatmap',
+    'create_demand_heatmap',
     'generate_trend_chart', 
     'generate_comparison_bar_chart',
+    'generate_ai_insights',
+    'get_performance_metrics',
     'compute_business_insights',
     'generate_insight_texts',
     'generate_recommendations'
